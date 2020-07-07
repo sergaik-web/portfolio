198c45008cf310fa72ac6c7b4b41a1f9e55ecd5f
sergaik-web <seroga8863@gmail.com>
2020-07-07 15:42:09 +0200

    Креате

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,20 +2,47 @@ import React from "react";
 import "./App.scss";
 import MenuList from "../MenuList";
 
-function App() {
-  return (
-    <div className="App">
-      <div className="fog">
-        <div></div>
-      </div>
-      <div id="app-window">
-        <div className={"bg-item"}></div>
-        <div className={"bg-item"}></div>
-        <div className={"bg-item"}></div>
-        <MenuList />
-      </div>
-    </div>
-  );
-}
+export default class App extends React.Component {
+  state = {
+    delta: 0,
+  };
 
-export default App;
+  componentDidMount() {
+    let delta = 0;
+    document.addEventListener("wheel", (e) => {
+      delta += e.deltaY || e.detail || e.wheelDelta;
+      if (delta < 0) {
+        delta = 0;
+      }
+      this.setState({ delta });
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="fog">
+          <div></div>
+        </div>
+        <div id="app-window">
+          <div
+            style={{ transform: "rotate(" + this.state.delta / 150 + "deg)" }}
+            className={"bg-item"}
+          ></div>
+          <div
+            style={{ transform: "rotate(" + this.state.delta / 100 + "deg)" }}
+            className={"bg-item"}
+          ></div>
+          <div
+            style={{
+              transform:
+                "rotate(" + Math.sin(this.state.delta / 10000) * 200 + "deg)",
+            }}
+            className={"bg-item"}
+          ></div>
+          <MenuList />
+        </div>
+      </div>
+    );
+  }
+}
--- src/Components/MenuList/menu-list.js
+++ src/Components/MenuList/menu-list.js
@@ -2,15 +2,32 @@ import React from "react";
 import "./menu-list.scss";
 
 export default class MenuList extends React.Component {
+  state = {
+    delta: 0,
+  };
+
   componentDidMount() {
     let delta = 0;
     document.addEventListener("wheel", (e) => {
       delta += e.deltaY || e.detail || e.wheelDelta;
-      console.log(delta);
+      if (delta < 0) {
+        delta = 0;
+      }
+      this.setState({ delta });
     });
   }
 
   render() {
-    return <div id={"left-panel"}></div>;
+    return (
+      <div
+        style={{
+          top: this.state.delta / 10 + "px",
+          left: this.state.delta / 10 + "px",
+        }}
+        id={"left-panel"}
+      >
+        {this.state.delta}
+      </div>
+    );
   }
 }
