b761c3b50481937bbbfd0489c625287c2ac3475d
sergaik-web <seroga8863@gmail.com>
2020-07-30 15:31:40 +0200

    test

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.scss";
 import LeftMenuList from "../left-menu-list";
 import RightMenuList from "../right-menu-list";
